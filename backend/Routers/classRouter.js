56a2020302b894b9b53bfbe88d241f3fad07f1e5
Tanya Dwivedi <you@example.com>
2024-04-11 22:07:13 +0530

    lets go

--- backend/Routers/classRouter.js
+++ backend/Routers/classRouter.js
@@ -32,6 +32,7 @@ router.delete('/delete/:id', (req, res) => {
    .then((result) => {
     res.status(200).json(result);
     })
+    
    .catch((err) => {
     console.log(err);
     res.status(500).json(err);
