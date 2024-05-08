4490d3fb16eb34fb3bbcd2f22e8bfaf6a551cce0
Tanya-Dwivedi08 <dwivedit78@gmail.com>
2024-05-08 13:12:10 +0530

    commit

--- frontend/src/app/(main)/lectures/page.jsx
+++ frontend/src/app/(main)/lectures/page.jsx
@@ -37,6 +37,7 @@ useEffect(() => {
              {lec.topic}
             </div>
           </div>
+          <div className="text-center"> 
           <a
                  href={'/viewLecture/' + lec._id}
                className="inline-flex items-center  px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -58,6 +59,7 @@ useEffect(() => {
                      />
                  </svg>
              </a>
+        </div>
         </div>
       </a>
     //     <div className="max-w-sm  bg-white border mb-5 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
